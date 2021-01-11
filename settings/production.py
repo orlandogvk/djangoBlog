cb4814737755087dd7b6ea972cedd769af664f35
Orlando <orlandog79@gmail.com>
2021-01-10 19:09:43 -0500

    Start deploy my blog

---
+++ settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'orlando12345',
+        'HOST': 'localhost',
+        'PORT': '5433'
+    }
+}
+
